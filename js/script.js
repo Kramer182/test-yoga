3b51045b021a9a1f28f722ae76923cfadac2fa38
Kramer182 <rollertoaster182@gmail.com>
2019-09-11 11:53:23 +0300

    ajax form

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ let descriptionBtn = document.querySelectorAll('.description-btn');
     }
    
         
-        
+    
         
     /*class Options {
         constructor(height, width, bg, fontSize, textAlign) {
@@ -125,5 +125,50 @@ let descriptionBtn = document.querySelectorAll('.description-btn');
     const item = new Options(300, 30, "blue", 14, "center");
     
     item.createDiv(); */
+    let message = {
+        loading: 'Загрузка.....',
+        success: 'Спасибо! Мы скоро с вами свяжемся',
+        failure: 'Что-то пошло не так.....'
+    };
     
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+    
+        statusMessage.classList.add('status');
+    
+        form.addEventListener('submit', function(event) {
+            event.preventDefault();
+            form.appendChild(statusMessage);
+        
+        let request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            request.setRequestHeader('Content-Type', 'application/json', 'charset=utf-8');
+    
+        let formData = new FormData(form);
+
+        let obj = {};
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+        let json = JSON.stringify(obj);
+        request.send(json);
+
+        request.addEventListener('readystatechange', function(){
+            if (request.readyState < 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 && request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+
+        for (let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+        
+    });
 }); 
+
+//form
